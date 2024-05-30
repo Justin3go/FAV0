365578700d41535009c9ee0b36646ff53f8b16b3
justin3go <just@justin3go.com>
2024-05-30 09:57:54 +0800

    perf: 预加载字体

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from "vue";
-import Theme from "vitepress/theme";
+import Theme from 'vitepress/theme-without-fonts' // https://vitepress.dev/zh/guide/extending-default-theme#using-different-fonts
 // 引入组件库的少量全局样式变量
 import 'tdesign-vue-next/es/style/index.css';
 
