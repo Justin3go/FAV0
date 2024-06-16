e19606d9d2791b1eb145e9e4ed215c0cd27a19f6
justin3go <just@justin3go.com>
2024-06-16 23:11:52 +0800

    docs: <weekly-003> 与GPT的信任危机

--- docs/.vitepress/utils/createSideBar.ts
+++ docs/.vitepress/utils/createSideBar.ts
@@ -4,6 +4,7 @@ export function createSideBarZH() {
       text: '2024年',
       collapsed: true,
       items: [
+        { text: '003期--与GPT的信任危机', link: '/posts/2024/003' },
         { text: '002期--独立开发的动力', link: '/posts/2024/002' },
         { text: '001期--AI内容污染搜索', link: '/posts/2024/001' },
         { text: '000期--新的开始', link: '/posts/2024/000' },
@@ -19,6 +20,7 @@ export function createSideBarEN() {
       text: 'The Year 2024',
       collapsed: true,
       items: [
+        { text: '003: The Trust Crisis with GPT', link: '/en/posts/2024/003' },
         { text: '002: The Motivation for indie Development', link: '/en/posts/2024/002' },
         { text: '001: AI Content Pollution in Search', link: '/en/posts/2024/001' },
         { text: '000: A New Beginning', link: '/en/posts/2024/000' },
