8bc79a71b9224a2f26a0280231d411fac931fc26
justin3go <just@justin3go.com>
2024-10-24 14:52:41 +0800

    chore: 增加follow验证

--- docs/.vitepress/utils/rss.ts
+++ docs/.vitepress/utils/rss.ts
@@ -8,7 +8,7 @@ const hostname = "https://fav0.com";
 export async function createRssFileZH(config: SiteConfig) {
   const feed = new Feed({
     title: "FAV0周刊",
-    description: "《FAV0周刊》：记录每周所见所闻，主要关注前端、AI领域以及计算机相关内容",
+    description: "《FAV0周刊》：记录每周所见所闻，主要关注前端、AI领域、独立开发以及计算机相关内容",
     id: hostname,
     link: hostname,
     language: "zh-Hans",
@@ -47,13 +47,25 @@ export async function createRssFileZH(config: SiteConfig) {
     });
   }
 
-  writeFileSync(path.join(config.outDir, "feed.xml"), feed.rss2(), "utf-8");
+  // 生成 RSS 字符串
+  let rssContent = feed.rss2();
+
+  // 插入自定义标签
+  const customTag = `
+<follow_challenge>
+    <feedId>54247025141998592</feedId>
+    <userId>41451588612609024</userId>
+</follow_challenge>  
+  `;
+  rssContent = rssContent.replace("</channel>", `${customTag}</channel>`);
+
+  writeFileSync(path.join(config.outDir, "feed.xml"), rssContent, "utf-8");
 }
 
 export async function createRssFileEN(config: SiteConfig) {
   const feed = new Feed({
     title: "FAV0 Weekly",
-    description: "FAV0 Weekly: Documenting Weekly Observations and Experiences, with a Focus on Front-end Development, AI, and Computer-related Topics",
+    description: "FAV0 Weekly: Documenting Weekly Observations and Experiences, with a Focus on Front-end Development, AI, independent developer, and Computer-related Topics",
     id: hostname,
     link: hostname,
     language: "en-US",
@@ -92,5 +104,17 @@ export async function createRssFileEN(config: SiteConfig) {
     });
   }
 
-  writeFileSync(path.join(config.outDir, "feed-en.xml"), feed.rss2(), "utf-8");
+  // 生成 RSS 字符串
+  let rssContent = feed.rss2();
+
+  // 插入自定义标签
+  const customTag = `
+  <follow_challenge>
+      <feedId>54247025141998592</feedId>
+      <userId>41451588612609024</userId>
+  </follow_challenge>  
+    `;
+  rssContent = rssContent.replace("</channel>", `${customTag}</channel>`);
+
+  writeFileSync(path.join(config.outDir, "feed-en.xml"), rssContent, "utf-8");
 }
